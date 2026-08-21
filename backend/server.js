c15528a53dc538af65cf25da270bb31f31173e04
Bubble Wrap <vishalpandey000090@gmail.com>
2026-08-22 00:33:11 +0530

    swapped to menifestation.surge.sh

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, "http://localhost:5173", "https://destiny.surge.sh"],
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173", "https://menifestation.surge.sh"],
   credentials: true,
 }));
 
@@ -32,7 +32,7 @@ app.use('/api/projects', projectRoutes);
 
 const io = new Server(server, {
   cors: {
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173", "https://destiny.surge.sh"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173", "https://menifestation.surge.sh"],
     credentials: true,
   }
 });
